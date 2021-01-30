f3a46c75c6fcc0a540c091cd353152b35040d782
sguerin <sguerin@guess-engineering.com>
2021-01-30 20:57:26 +0100

    elsa dashboard added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # poc-state-machine-elsa
+
+Make sure to first run the SqlServer migrations against your SQL Server:
+
+```
+SET EF_CONNECTIONSTRING=Server=(localdb)\\mssqllocaldb;Database=PocDB;Trusted_Connection=True;
+dotnet ef database update --context SqlServerContext
+```
--- src/Poc/Dashboard/Startup.cs
+++ src/Poc/Dashboard/Startup.cs
@@ -7,15 +7,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Elsa.Activities.Email.Extensions;
+using Elsa.Activities.Http.Extensions;
+using Elsa.Activities.Timers.Extensions;
+using Elsa.Dashboard.Extensions;
+using Elsa.Persistence.EntityFrameworkCore.DbContexts;
+using Elsa.Persistence.EntityFrameworkCore.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Dashboard
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                // Add services used for the workflows runtime.
+                .AddElsa(elsa => elsa.AddEntityFrameworkStores<SqlServerContext>(options => options.UseSqlServer(Configuration.GetConnectionString("db"))))
+                .AddHttpActivities(options => options.Bind(Configuration.GetSection("Elsa:Http")))
+                .AddEmailActivities(options => options.Bind(Configuration.GetSection("Elsa:Smtp")))
+                .AddTimerActivities(options => options.Bind(Configuration.GetSection("Elsa:Timers")))
+
+                // Add services used for the workflows dashboard.
+                .AddElsaDashboard();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +51,11 @@ namespace Dashboard
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseStaticFiles()
+               .UseHttpActivities()
+               .UseRouting()
+               .UseEndpoints(endpoints => endpoints.MapControllers())
+               .UseWelcomePage();
         }
     }
 }
