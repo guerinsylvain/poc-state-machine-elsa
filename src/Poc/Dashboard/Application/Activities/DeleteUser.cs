5f9fa328c748c56019acce0711e4da574c299c77
sguerin <sguerin@guess-engineering.com>
2021-02-03 17:18:50 +0100

    empty delete user activity added

---
+++ src/Poc/Dashboard/Application/Activities/DeleteUser.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Elsa.Attributes;
+using Elsa.Expressions;
+using Elsa.Results;
+using Elsa.Services;
+using Elsa.Services.Models;
+
+namespace Dashboard.Application.Activities
+{
+    [ActivityDefinition(Category = "Users", Description = "Delete a User", Icon = "fas fa-user-minus", Outcomes = new[] { DeleteUserOutcomes.Deleted, DeleteUserOutcomes.NotFound })]
+    public class DeleteUser : Activity
+    {
+        [ActivityProperty(Hint = "Enter an expression that evaluates to the ID of the user to activate.")]
+        public WorkflowExpression<string> UserId
+        {
+            get => GetState<WorkflowExpression<string>>();
+            set => SetState(value);
+        }
+
+        protected override async Task<ActivityExecutionResult> OnExecuteAsync(WorkflowExecutionContext context, CancellationToken cancellationToken)
+        {
+            var userId = await context.EvaluateAsync(UserId, cancellationToken);
+            
+            //TODO: try to delete user
+
+            return Outcome(DeleteUserOutcomes.Deleted);
+        }
+
+    }
+}
---
+++ src/Poc/Dashboard/Application/Activities/DeleteUserOutcomes.cs
@@ -0,0 +1,8 @@
+﻿namespace Dashboard.Application.Activities
+{
+    public class DeleteUserOutcomes
+    {
+        public const string NotFound = "NotFound";
+        public const string Deleted = "Deleted";
+    }
+}
